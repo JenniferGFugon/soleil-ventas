9f4a7bca95e4657c92e60ea13b216d547085e73d
Jennifer Guerrero Fugón <61253017+JenniferGFugon@users.noreply.github.com>
2025-10-20 09:58:58 -0600

    cambio

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,15 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const db = require('./db');
 
+// Servir archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Si el usuario visita cualquier ruta no definida, devuelve index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
